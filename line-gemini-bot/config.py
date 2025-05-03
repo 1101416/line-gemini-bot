fb2accaa95fe44fdcf1d87524c7b15ac8bb45648
1101416 <103500373+1101416@users.noreply.github.com>
2025-05-03 16:21:04 +0800

    Update config.py

--- line-gemini-bot/config.py
+++ line-gemini-bot/config.py
@@ -1,6 +1,6 @@
 # config.py
 import os
 
-LINE_CHANNEL_ACCESS_TOKEN = os.environ.get('6mhCk1ED6wThH7g4oztPN4WKK8zSCND5jZ/vIi5MvwUEJLbwItbCScKOjPeri9prom5UK/dzrWlblTqJHFxKZ7A2dtK4PVZNi+Gp/SFY65CpGv0cdE6AqaX8D1jbHvihylpHdR9coxNpyTlA1p62GwdB04t89/1O/w1cDnyilFU=')
-LINE_CHANNEL_SECRET = os.environ.get('0dc099bcec7e4ff57f8e5251ff1a9748')
-GEMINI_API_KEY = os.environ.get('AIzaSyC9q9XCyrurFHX-YTFRSan7MRKTFmGF7wA')
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('6mhCk1ED6wThH7g4oztPN4WKK8zSCND5jZ/vIi5MvwUEJLbwItbCScKOjPeri9prom5UK/dzrWlblTqJHFxKZ7A2dtK4PVZNi+Gp/SFY65CpGv0cdE6AqaX8D1jbHvihylpHdR9coxNpyTlA1p62GwdB04t89/1O/w1cDnyilFU=')
+LINE_CHANNEL_SECRET = os.getenv('0dc099bcec7e4ff57f8e5251ff1a9748')
+GEMINI_API_KEY = os.getenv('AIzaSyC9q9XCyrurFHX-YTFRSan7MRKTFmGF7wA')
